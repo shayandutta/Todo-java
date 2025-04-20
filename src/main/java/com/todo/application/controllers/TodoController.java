5a822c7e829a0bc4892924df32e18e7514a085b8
shayandutta <shayandutta23@gmail.com>
2025-04-20 22:33:13 +0530

    handling status code

--- src/main/java/com/todo/application/controllers/TodoController.java
+++ src/main/java/com/todo/application/controllers/TodoController.java
@@ -5,6 +5,8 @@ import com.todo.application.services.TodoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,14 +32,14 @@ public class TodoController {
 
     //create todo objects from json data
     @PostMapping
-    public Todo todoHandler(@RequestBody Todo todo){
+    public ResponseEntity<Todo> todoHandler(@RequestBody Todo todo){
         //create todo
-        int id = random.nextInt(999999);
+        int id = random.nextInt(999999); //generating the random id
         todo.setId(id); //whenever a new todo comes, this random id will be set to the todo via this todo setter method
         logger.info("Create Todo");
 
         //call service to create todo
         Todo todo1 = todoService.createTodo(todo);
-        return todo1;
+        return new ResponseEntity<>(todo1, HttpStatus.CREATED);
     }
 }
